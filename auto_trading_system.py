537c1512c5a015418db7a228a98e5bb79c4b549f
hanbee.choi <hubaiwoong@proton.me>
2024-06-17 14:46:15 +0900

    [feature] traiding

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -15,6 +15,32 @@ class AutoTradingSystem:
     def sell(self, stock_code, count, price):
         self._driver.sell(stock_code, count, price)
 
+    def buy_nice_timing(self, stock_code, cash_amount):
+        if self.is_bullish(stock_code):
+            current_price = self.get_price(stock_code)
+            max_amount = int(cash_amount / current_price)
+            self.buy(stock_code, max_amount, current_price)
+
+    def sell_nice_timing(self, stock_code, stock_amount):
+        if self.is_bearish(stock_code):
+            current_price = self.get_price(stock_code)
+            self.sell(stock_code, stock_amount, current_price)
+
+    def is_bullish(self, stock_code):
+        tick_1 = self.get_price(stock_code)
+        tick_2 = self.get_price(stock_code)
+        tick_3 = self.get_price(stock_code)
+        if tick_1 < tick_2 and tick_2 < tick_3:
+            return True
+        return False
+
+    def is_bearish(self, stock_code):
+        tick_1 = self.get_price(stock_code)
+        tick_2 = self.get_price(stock_code)
+        tick_3 = self.get_price(stock_code)
+        if tick_1 > tick_2 and tick_2 > tick_3:
+            return True
+        return False
 
     @property
     def stock_broker_driver(self):
