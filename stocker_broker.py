4f21c77bf36cddd98c401e24411959aa9f3d104c
shpark <sh0913.park@samsung.com>
2024-06-17 14:19:21 +0900

    refactoring: merge unittests

--- stocker_broker.py
+++ stocker_broker.py
@@ -32,6 +32,17 @@ class KiwerDriver(StockerBrokerDriverInterface):
     def sell(self, stock_code, count, price):
         self._kiwer_api.sell(stock_code, count, price)
 
+
 class NemoDriver(StockerBrokerDriverInterface):
     def __init__(self, api: NemoAPI):
+        super().__init__()
         self.api = api
+
+    def login(self, id, password):
+        pass
+
+    def buy(self, stock_code, count, price):
+        pass
+
+    def sell(self, stock_code, count, price):
+        pass
